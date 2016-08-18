4f780d5c28afcce74cc2296db0844ba034ae89ae
Gleidson FerSanP <gleidsonfersanp@gmail.com>
2016-08-17 23:04:47 -0300

    ccc

--- src/main/webapp/js/app/config.js
+++ src/main/webapp/js/app/config.js
@@ -1,4 +1,4 @@
 angular.module('config', []).constant("Config", {
-    "baseUrl": "http://localhost:8080"
-    //"baseUrl": "http://45.55.134.116/servicos"
+    //"baseUrl": "http://localhost:8080"
+    "baseUrl": "http://45.55.134.116/servicos"
 });
